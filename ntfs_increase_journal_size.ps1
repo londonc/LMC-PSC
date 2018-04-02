bbd06cd36042dac1d8238cb71c126a4519d2eba0
London Crosby <london.crosby@gmail.com>
2018-04-02 17:13:32 -0400

    Create ntfs_increase_journal_size.ps1

---
+++ ntfs_increase_journal_size.ps1
@@ -0,0 +1,34 @@
+# Increase NTFS USN journal size
+# Author: London Crosby v0.5
+
+# Set volume. Default is C:
+$Volume = C:
+
+#Set as MB. Default is 32MB.
+$DesiredSize = 1000
+
+# Maths needed for conversion
+$DesiredSizeInBytes = $DesiredSize * 1024 * 1024
+$DesiredSizeInHex = "{0:x16}" -f $DesiredSizeInBytes
+
+# Recommed setting Allocation Delta to around 20%
+$AllocationDeltaInBytes = [math]::round($DesiredSizeInBytes * 0.2)
+$AllocationDeltaInHex = [Convert]::ToString($AllocationDeltaInBytes, 16)
+
+
+$GetUsnMaxSize = fsutil.exe usn queryjournal $Volume
+
+foreach ($o in $GetUsnMaxSize){
+   if ($o.StartsWith("Maximum Size")){
+       # Split the output
+       $oSetSize = $o -Split("0x")
+       
+       # Check if current value already matches desired size
+       if ($oSetSize[1] -ne $DesiredSizeInHex) {
+          # Set size otherwise
+          fsutil usn createjournal m=$DesiredSizeInHex a=$AllocationDeltaInHex $Volume
+       }
+
+   }
+
+} 
