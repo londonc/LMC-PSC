70962e2e76b8fcce1a2b7fc7972fa7edc6766d75
London Crosby <london.crosby@gmail.com>
2018-06-26 14:25:56 -0400

    Update o365_alert_new_inbox_rule.ps1
    Add check or From address. Need to add check for PSv5 or greater.

--- o365_alert_new_inbox_rule.ps1
+++ o365_alert_new_inbox_rule.ps1
@@ -15,25 +15,36 @@ If (!(Test-Path $ExoPassFile)) {
     $GetPass = Read-Host -Prompt "Enter password:" -AsSecureString
     $GetPass | ConvertFrom-SecureString | Out-File $ExoPassFile 
 }
-# 
+
 $ExoCredentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ExoLogin, (Get-Content $ExoPassFile | ConvertTo-SecureString)
 
 $ExoSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/PowerShell/ -Credential $ExoCredentials -Authentication Basic -AllowRedirection
 
 Import-PSSession $ExoSession
+# For future functionality
 Import-Module MsOnline
 Connect-MsolService -Credential $ExoCredentials
 
-# Set basic Alerting variables
+# Set basic alerting variables
 $AlertTo = 'your_address@example.com'
 $AlertWhat = 'New-InboxRule'
 $AlertSMTP = 'smtp-relay.example.com'
+$BlacklistContains = 'hack'
+$BlacklistFromAddress = 'postmaster@'
 
-# Add functionaly to pass argument in future
+# Add functionaly to pass argument
 $SearchTime = Get-Date
 
 # Default is 100 results returned. Not common to exceed but if shit hits the fan then or big userbase at least you get a better picture
 $AuditLog = Search-UnifiedAuditLog -StartDate $SearchTime.AddMinutes(-15) -EndDate $SearchTime -Operations $AlertWhat -ResultSize 5000 | Select CreationDate, UserIds, Operations,AuditData,ResultIndex,ResultCount
+
+function SendAlert {
+    $AlertSubject = "Suspicious $AlertWhat Activity for $aUserId!"
+    
+    # Using HTML so it can be expanded on in the future with pretty colors for idiots
+    $AlertBody = "<h3>$aTime - $aUserId ($aUserName) from $aUserIp</h3><p>Created a $AlertWhat that contains: $elementValue</p></br><p style=font-weight:bold;>INVESTIGATE ASAP!!!</p>"
+    Send-MailMessage -From $ExoLogin -To $AlertTo -Subject $AlertSubject -BodyAsHtml $AlertBody -SmtpServer $AlertSMTP 
+}
 
 foreach ($l in $AuditLog){
     # 
@@ -41,21 +52,15 @@ foreach ($l in $AuditLog){
     $aTime = $aData.CreationTime
     $aUserId = $aData.UserId
     $aUserIp = $aData.ClientIP
-    # Get user details - can be used for future expansion like blocking sign ins
     $aUserDetails = Get-MsolUser -UserPrincipalName $aUserId
     $aUserName = $aUserDetails.DisplayName
     $aParams = $aData.Parameters
     #
-    if ($aParams -match 'SubjectOrBodyContainsWords'){
+    if ($aParams.Name -match 'SubjectOrBodyContainsWords' -Or $aParams.Name -match 'FromAddressContainsWords'){
         foreach ($element in $aParams) {
         $elementValue = $element.Value
-            if ($elementValue.Contains('hack')){
-
-                $AlertSubject = "$AlertWhat - $aUserId!"
-                # Using HTML so it can be expanded on in the future with pretty colors for idiots
-                $AlertBody = "<h3>$aTime - $aUserId ($aUserName) from  $aUserIp</h3><p>Created a new inbox rule that contains: $elementValue</p></br><p style=font-weight:bold;>INVESTIGATE ASAP!!!</p>"
-
-                Send-MailMessage -From $ExoLogin -To $AlertTo -Subject $AlertSubject -BodyAsHtml $AlertBody -SmtpServer $AlertSMTP
+            if ($elementValue.Contains($BlacklistContains) -Or $elementValue.Contains($BlacklistFromAddress)){
+                SendAlert
             }
         }
     }
