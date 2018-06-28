c900f0ef85eaabe1d65e531356e5d9714447f05a
London Crosby <london.crosby@gmail.com>
2018-06-28 12:34:42 -0400

    Update o365_seek_n_destroy.ps1
    More guard rails and error handling. Still some small bugs.

--- o365_seek_n_destroy.ps1
+++ o365_seek_n_destroy.ps1
@@ -8,27 +8,33 @@ Powershell script to search through Office 365 Compliance Center and delete emai
 $CurrentDate = Get-Date
 $SearchDate = $CurrentDate.ToString()
 
+
 Write-Output '### SEARCH AND REMOVE EMAILS MATCHING THE FOLLOWING CRITERIA ###'
-$SearchParamFrom = Read-Host -Prompt 'Email from'
+$SearchParamFrom = Read-Host -Prompt 'From'
 $SearchName = "$SearchParamFrom - $SearchDate"
 
 # Prevent user from accidentially entering in nothing. 
 $SearchParamSubject = ""
 While (!$SearchParamSubject){
-    $SearchParamSubject = Read-Host -Prompt 'Email Subject Line'  
+    $SearchParamSubject = Read-Host -Prompt 'Subject Line'  
 }
 
-#
+# Seen cases where '!' causes a problem
 If ($SearchParamSubject.Contains("!")){
-    Write-Output "WARNING: Contains special character that may present a problem... removing from string"
-    # '!' character can cause problems. Probably ASCII/Unicode handling issue. 
+    Write-Output 'WARNING: Contains special character that may present a problem... replacing with wildcard'
     $SearchParamSubject = $SearchParamSubject -replace '!','*'
 }
 
-# Narrow the scope a bit
-$SearchParamDate = Read-Host -Prompt 'Email sent after (ie YYYY-MM-DD - Default 1 Month)'
-If (!$SearchParamDate) { 
-    $SearchParamDate = $CurrentDate.AddMonths(-1).ToString('yyy-MM-dd')
+# Narrow the scope a bit with some guard rails
+$SearchParamDate = ""
+While (($SearchParamDate.Length -lt 10) -or ($SearchParamDate.Length -gt 10)){
+    $SearchParamDate = Read-Host -Prompt 'Sent after YYYY-MM-DD (Default 1 Month)'
+
+    If ($SearchParamDate.Length -eq 0) { 
+        $SearchParamDate = $CurrentDate.AddMonths(-1).ToString('yyy-MM-dd')
+    } else { 
+        Write-Output 'ERROR: Date format incorrect. Must be in YYYY-MM-DD format, or leave blank for default. '
+    }
 }
 
 $EOLogin = 'admin@example.com'
@@ -46,7 +52,12 @@ $EOCCSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUr
 Import-PSSession $EOCCSession
 New-ComplianceSearch -Name "$SearchName" -ContentMatchQuery "(c:c)(subjecttitle:$SearchParamSubject)(from:$SearchParamFrom)(sent>$SearchParamDate)‎" -Description "Initiated from $env:COMPUTERNAME" -ExchangeLocation All -AllowNotFoundExchangeLocationsEnabled $true
 
-Start-ComplianceSearch -Identity "$SearchName"
+function EOL {
+    Write-Output '### END OF LINE ###'
+    Remove-PSSession $EOCCSession
+}
+
+Start-ComplianceSearch -Identity "$SearchName";
 
 # Be nice and wait for search to complete 
 Write-Output '### WAITING FOR SEARCH TO COMPLETE... ###'
@@ -56,21 +67,34 @@ While ($SearchProgress.Status -ne 'Completed'){
     $SearchProgress = Get-ComplianceSearch -Identity "$SearchName" | Select Items, Status
 }
 
+Write-Output '### SEARCH COMPLETE ###'
+
+
+# Get error count for preventing rolling errors
+$ErrorCount = $Error.Count
+
 If($SearchProgress.Items -gt 0){
+    Write-Output "### ITEMS FOUND. STARTING PURGE... ###"
     New-ComplianceSearchAction -SearchName "$SearchName" -Purge -PurgeType SoftDelete -Verbose
 
-    # Wait and check progress of purge 
-    $PurgeProgress = Get-ComplianceSearchAction -Identity $SearchName"_purge"
-    While($PurgeProgress.Status -ne 'Completed'){
-        Start-Sleep -s 1
         $PurgeProgress = Get-ComplianceSearchAction -Identity $SearchName"_purge"
-    }
-    Write-Out "$SearchProgress.Items deleted"
+
+        
+        If($ErrorCount -eq $Error.Count){
+            While($PurgeProgress.Status -ne 'Completed'){
+               Write-Output '### WAITING FOR PURGE TO COMPLETE... ###'
+                Start-Sleep -s 1
+                $PurgeProgress = Get-ComplianceSearchAction -Identity $SearchName"_purge"
+            }
+            Write-Out "$SearchProgress.Items deleted"
+
+        } else {
+            Write-Output "ERROR: User may have selected not to purge or something went wrong. See output for debugging. "
+            EOL
+        }
 
 } else {
     Write-Output "ERROR: No items found! Check your search parameters. "
 }
 
-# EOL
-Write-Output '### END OF LINE ###'
-Remove-PSSession $EOCCSession
+EOL
